fcb4669a271b34c6b533cbd4a832518f1913444d
akhmelev <akhmelev@gmail.com>
2024-05-31 22:03:06 +0300

    lesson 15. add Inheritance sample (refactor)

---
+++ src/main/java/com/javarush/lesson15/Database.java
@@ -0,0 +1,52 @@
+package com.javarush.lesson15;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+enum Database {
+
+    SESSION;
+
+    private Session session;
+
+    private final SessionFactory sessionFactory;
+
+    Session open() {
+        session = sessionFactory.openSession();
+        return session;
+    }
+
+    void close() {
+        session.close();
+    }
+
+    void closeFactory() {
+        sessionFactory.close();
+    }
+
+    Database() {
+        LogManager logManager = LogManager.getLogManager();
+        Logger logger = logManager.getLogger("");
+        logger.setLevel(Level.WARNING);
+
+        Configuration configure = new Configuration()
+                .setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/game")
+                .setProperty("hibernate.connection.username", "postgres")
+                .setProperty("hibernate.connection.password", "postgres")
+                .setProperty("hibernate.connection.driver_class", "org.postgresql.Driver")
+                .setProperty("hibernate.show_sql", "true")
+                .setProperty("hibernate.format_sql", "true")
+                .setProperty("hibernate.hbm2ddl.auto", "create");
+        //.configure(); //skip hibernate.cfg.xml
+        //configure.addAnnotatedClass(????.class);
+        configure.addAnnotatedClass(BaseParent.class);
+        configure.addAnnotatedClass(Customer.class);
+        configure.addAnnotatedClass(Seller.class);
+        sessionFactory = configure.buildSessionFactory();
+    }
+}
